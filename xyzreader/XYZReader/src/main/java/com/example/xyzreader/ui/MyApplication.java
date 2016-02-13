b633baca1761a80b58e73cd48c33f71126fc43e7
pawankumar <pawan.kumar@mediaagility.com>
2016-02-13 16:07:58 +0530

    XYZ Reader According to Material Design

---
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/MyApplication.java
@@ -0,0 +1,30 @@
+package com.example.xyzreader.ui;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by pawankumar on 02/02/16.
+ */
+public class MyApplication extends Application {
+    private static MyApplication mInstance;
+    private static Context mAppContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        this.setAppContext(getApplicationContext());
+    }
+
+    public static MyApplication getInstance(){
+        return mInstance;
+    }
+    public static Context getAppContext() {
+        return mAppContext;
+    }
+    public void setAppContext(Context mAppContext) {
+        this.mAppContext = mAppContext;
+    }
+}
